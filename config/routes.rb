bd3585783d630a39861b371bfe368988dc8ca494
Balestrini Bernardo <balestrinibernardo@gmail.com>
2021-11-24 15:49:41 +0100

    DeleteTaskX

--- config/routes.rb
+++ config/routes.rb
@@ -3,14 +3,8 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   get 'dashboard', to: 'pages#dashboard'
   post 'work_session/:id', to: 'work_sessions#create', as: 'work_sessions'
-<<<<<<< HEAD
   resources :tasks, only: [:create, :update, :destroy] do
     resources :timeboxes, only: [:create]
-=======
-  resources :tasks, only: [:create, :update]
-  resources :work_sessions, only: [:show] do
-    resources :timeboxes, only: [:create, :update]
->>>>>>> 1ce4c423ea36b77b4a5a0955812d314e871c6075
   end
   delete 'timeboxes/:id', to: 'timeboxes#destroy', as: 'timeboxes'
   get 'work_session/:id', to: 'work_sessions#show'

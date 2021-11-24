5196e410696b88485bd4d0d265ab1a726a6bc096
Ting Ting Chen <tin2294@gmail.com>
2021-11-24 11:58:26 +0100

    modal

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,12 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   get 'dashboard', to: 'pages#dashboard'
   post 'work_session/:id', to: 'work_sessions#create', as: 'work_sessions'
-  resources :tasks, only: [:create] do
+  resources :tasks, only: [:create, :update] do
     resources :timeboxes, only: [:create]
   end
   get 'work_session/:id', to: 'work_sessions#show'
   patch 'work_session/:id', to: 'work_sessions#mark_as_done'
+  patch 'task/:id', to: 'tasks#update'
   patch 'timeboxes/:id', to: 'timeboxes#start'
   patch 'timeboxes/:id', to: 'timeboxes#end'
   patch 'timeboxes/:id', to: 'timeboxes#update'

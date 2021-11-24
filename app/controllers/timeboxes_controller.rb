5fc615fd557ec19fe2c59e9a84e43d7942c9a945
Ting Ting Chen <tin2294@gmail.com>
2021-11-24 17:43:26 +0100

    arreglado el view

--- app/controllers/timeboxes_controller.rb
+++ app/controllers/timeboxes_controller.rb
@@ -1,17 +1,18 @@
 class TimeboxesController < ApplicationController
   def create
-    @work_session = WorkSession.find(params[:work_session_id])
+    @task = Task.find(params[:task_id])
+    @work_session = @task.work_session
     @timebox = Timebox.new(time_params)
     @timebox.task = @work_session.task
     @timebox.break = true if params[:break] == "1"
     @timebox.save
-    redirect_to @work_session
+    redirect_to work_sessions_path(@work_session)
   end
 
   def destroy
     @timebox = Timebox.find(params[:id])
     @timebox.destroy
-    redirect_to work_session_path(@timebox.task.work_session)
+    redirect_to work_sessions_path(@timebox.task.work_session)
   end
 
   def start

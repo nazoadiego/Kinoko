282d4c04d1597f1685d29cb7535292ca60540928
Ting Ting Chen <tin2294@gmail.com>
2021-11-30 12:44:04 +0100

    added labels

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,8 @@
 class Task < ApplicationRecord
   has_one :work_session, dependent: :destroy
   has_many :timeboxes, dependent: :destroy
+  has_many :task_labels, dependent: :destroy
+  has_many :labels, through: :task_labels
   belongs_to :user
 
   validates :title, uniqueness: true

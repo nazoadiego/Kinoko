2938d588914633618dd235603c5c77451e21f6b2
diegomarrufo <dmarrufo@ucm.es>
2021-12-17 15:47:01 +0100

    trying a different method from pascal using this https://dev.to/junko911/make-your-rails-application-seo-friendly-by-setting-meta-tags-6o5

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,23 @@
 module ApplicationHelper
+  def default_meta_tags
+    {
+      site: 'kinoko.app',
+      title: 'Kinoko',
+      reverse: true,
+      separator: '|',
+      description: 'Timeboxing app with time logging and a mushroom forest',
+      keywords: 'timeboxing, pomodoro, productivity',
+      canonical: request.original_url,
+      noindex: !Rails.env.production?,
+      icon: [],
+      og: {
+        site_name: 'kinoko.app',
+        title: 'Kinoko',
+        description: 'Timeboxing app with time logging and a mushroom forest',
+        type: 'website',
+        url: request.original_url,
+        image: image_url('cover.png')
+      }
+    }
+  end
 end

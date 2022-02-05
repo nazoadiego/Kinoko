e3854f8dbfa39d81d0070f0b9dfbc62aa4ad8b50
diegomarrufo <dmarrufo@ucm.es>
2022-02-06 00:41:00 +0100

    lots of css cleaning, removed bootstrap, added tailwind. Suffered.

--- app/javascript/plugins/init_sortable.js
+++ app/javascript/plugins/init_sortable.js
@@ -1,11 +1,11 @@
-import Sortable from 'sortablejs';
+import Sortable from "sortablejs";
 
 const initSortable = () => {
-  let list = document.querySelector('.task-grid');
+  let list = document.querySelector(".task-grid");
   if (list) {
     Sortable.create(list);
-    console.log(Sortable.create(list))
-  };
+    console.log(Sortable.create(list));
+  }
 };
 
 export { initSortable };
